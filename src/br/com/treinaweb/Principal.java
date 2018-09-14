e49b0b4c8197c23593427aea49748edc8a372a5e
@andradeallan <allanpctec@gmail.com>
2018-09-14 13:01:09 -0300

    Utilizando operadores relacionais final

--- src/br/com/treinaweb/Principal.java
+++ src/br/com/treinaweb/Principal.java
@@ -3,22 +3,14 @@ package br.com.treinaweb;
 public class Principal {
 
     public static void main (String[] args) {
-
         int a = 3;
         int b = 5;
-        double resultadoDivisao = a / 5.0;
-//        int c = 5;
-//        int resultado = (a * b) + c;
-//        System.out.println("O resultado de " + a + " x " + b + " + "+ c +" é " + resultado);
-//        System.out.printf("O resultado de %d x %d + %d é %d", a, b, c, resultado);
-        System.out.printf("%d + %d = %d%n", a, b, a + b);
-        System.out.printf("%d - %d = %d%n", a, b, a - b);
-        System.out.printf("%d x %d = %d%n", a, b, a * b);
-        System.out.printf("%d / %d = %d%n", a, b, a / b);
-        System.out.printf("%d módulo %d = %d%n", a, b, a % b);
-        System.out.printf("%d / %d = %f%n", a, b, resultadoDivisao);
+        System.out.printf("%d > %d = %b%n", a, b, a > b);
+        System.out.printf("%d < %d = %b%n", a, b, a < b);
+        System.out.printf("%d <= %d = %b%n", a, b, a <= b);
+        System.out.printf("%d >= %d = %b%n", a, b, a >= b);
+        System.out.printf("%d = %d = %b%n", a, b, a == b);
+        System.out.printf("%d <> %d = %b%n", a, b, a != b);
+
     }
 }
-
-// git config --global user.email "allanpctec@gmail.com"
-// git config --global user.name "@andradeallan"
